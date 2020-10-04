961e6dfdc0c2fbd0eceed9d2ada51c9cb7982fca
Jorge Ivan Guevara Perez <jorgei.guevarap@utadeo.edu.co>
2020-10-03 20:45:10 -0500

    v 2.0.2

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '%luoc+y7940t@+*&sa)*61rs(s+d44@tt9zu4*d5fhb@v)yoo0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
